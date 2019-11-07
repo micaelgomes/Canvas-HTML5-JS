7ee54cd2d0293a42b1b6bb5fd86cf945b54e7560
micaelgoms <micaellgoms@gmail.com>
2019-11-06 21:52:18 -0300

    Algs pick implementados; Readme escrito

--- draw.js
+++ draw.js
@@ -3,11 +3,14 @@
 * @author micaellgoms 
 */
 
-
 var canvas = document.getElementById('canvas');
 var ctx = canvas.getContext('2d');
 
 var pathClicks = [];
+
+let pPoint = false;
+let pLine = false;
+let pPolygon = false;
 
 let polygon = false;
 let line = false;
@@ -21,6 +24,7 @@ let escale = false;
 let enable_draw = true;
 let enable_real = false;
 let witdthPoint = 2;
+let tol = 10;
 
 document.getElementById('canvas').addEventListener('click', mouseLeftClick, false);
 
@@ -126,15 +130,6 @@ function drawPolygonRealTime(){
   }
 }
 
-function pickPonto(x1, y1, d, x2, y2){
-  if( ((x2 <= (x1 + d)) && (x2 >= (x1 - d))) && ((y2 <= (y1 + d)) && (y2 >= (y1 - d))) ){
-    console.log("pick");
-    return true;
-  }
-
-  return false;
-}
-
 /*
  * Draw rectangle in canvas
  * @param color and array with (x, y) and base, height
@@ -164,7 +159,16 @@ function clearAll(){
   line = false;
   circle = false;
   bezier = false;
+  pPoint = false;
+  pLine = false;
+  pPolygon = false;
+  translate = false;
+  rotate = false;
+  escale = false;
+  ctx.strokeStyle = '#000000';
   document.getElementById('real-polygon').style.backgroundColor = "#666";
+  location.reload();
+
 }
 
 function clearScreen(){
@@ -242,6 +246,38 @@ function mouseLeftClick(event){
     }
     ctx.restore();
     escale = false;
+  }
+
+  if(pPoint){
+    for(let i=0; i<pathClicks.length; i++){
+      if(pickPonto(positionLastClick[0], positionLastClick[1], tol, pathClicks[i][0], pathClicks[i][1])){
+        // alert("Point selected!!!!");
+        ctx.strokeStyle = '#FF0000';
+        ctx.arc(pathClicks[i][0]+1, pathClicks[i][1]+1, 4, 0, 2 * Math.PI);
+        // ctx.closePath();
+        ctx.stroke();
+        break;
+      }
+    }
+  }
+
+  if(pLine){
+    // pickLine(x0, y0, x1, y1, cx, cy, tol)
+    for(let i=0; i<(pathClicks.length-1); i++){
+      if(pickLine(pathClicks[i][0], pathClicks[i][1], pathClicks[i+1][0], pathClicks[i+1][1], positionLastClick[0], positionLastClick[1], tol)){
+        console.log('pickLine!');
+        ctx.strokeStyle = '#FF0000';
+        drawLine(); 
+      }
+    }
+  }
+
+  if(pPolygon){
+    if(pickPoligono(positionLastClick[0], positionLastClick[1], pathClicks)){
+      console.log('inside Polygon!');
+      ctx.strokeStyle = '#FF0000';
+      drawPolygon(); 
+    }
   }
 }
 
@@ -349,13 +385,16 @@ function selection(option){
   if(!pathClicks.length < 1){
     if (option === "point"){
       alert("pickPoint selecionado!\nclick para selecionar");
-      selectPoint();
+      pPoint = true;
+      enable_draw = false;
     } else if (option === "line") {
       alert("pickLine selecionado!\nclick para selecionar");
-      selectPoint();
+      pLine = true;
+      enable_draw = false;
     } else if (option === "polygon") {
       alert("pickPolygon selecionado!\nclick para selecionar");
-      selectPoint();
+      pPolygon = true;
+      enable_draw = false;
     }
 
   } else {
@@ -363,6 +402,115 @@ function selection(option){
   }
 }
 
-function selectPoint(){
+/**
+ *  PICK - FUNCTIONS
+ */
 
+function pickPonto(x1, y1, d, x2, y2){
+  if( ((x2 <= (x1 + d)) && (x2 >= (x1 - d))) && ((y2 <= (y1 + d)) && (y2 >= (y1 - d))) ){
+      // console.log("pick");
+      return true;
+  }
+
+  return false;
+}
+
+function pickPoligono(mx, my,vetor_pontos_obj) {
+  let ni = 0;
+  let fst = vetor_pontos_obj.length -1;
+  let xc;
+  let p1, p2;
+
+  for (let i = 0; i <vetor_pontos_obj.length; i++) {
+      p1 = vetor_pontos_obj[i];
+      p2 = vetor_pontos_obj[fst];
+
+      if (!(p1[1] == p2[1]) && !((p1[1] > my) && (p2[1] > my)) &&
+          !((p1[1] < my) && (p2[1] < my)) && !((p1[0] < mx) && (p2[0] < mx))) {
+          if (p1[1] == my) {
+              if ((p1[0] > mx) && (p2[1] > my))
+                  ni++;
+          } else {
+              if (p2[1] == my) {
+                  if ((p2[0] > mx) && (p1[1] > my))
+                      ni++;
+              } else {
+                  if ((p1[0] > mx) && (p2[0] > mx)) {
+                      ni++;
+                  } else {
+                      let dx = p1[0] - p2[0];
+                      xc = p1[0];
+                      if (dx != 0) {
+                          xc += (my - p1[1]) * dx / (p1[1] - p2[1]);
+                          if (xc > mx)
+                              ni++;
+                      }
+                  }
+              }
+          }
+      }
+      fst = i;
+  }
+  return (ni%2!=0);
+}
+
+function binCode(x, y, xmin, xmax, ymin, ymax) {
+  var cod = [0, 0, 0, 0];
+  if (x < xmin)
+      cod[0] = 1;
+  if (x > xmax)
+      cod[1] = 1;
+  if (y > ymax)
+      cod[2] = 1;
+  if (y < ymin)
+      cod[3] = 1;
+  return cod;
+}
+
+// c -> mouse coordinate
+function pickLine(x0, y0, x1, y1, cx, cy, tol) {
+  var j;
+  var cod0 = [];
+  var cod1 = [];
+  var xmin, xmax, ymin, ymax;
+
+  xmin = cx - tol;
+  xmax = cx + tol;
+  ymin = cy - tol;
+  ymax = cy + tol;
+
+  cod1 = binCode(x1, y1, xmin, xmax, ymin, ymax);
+
+  do {
+      cod0 = binCode(x0, y0, xmin, xmax, ymin, ymax);
+
+      for (j = 0; j < 4; j++) {
+          if (cod0[j] && cod1[j]) {
+              break;
+          }
+      }
+      if (j != 4) {
+          break;
+      }
+      if (cod0[0]) {
+          y0 += (xmin - x0) * (y1 - y0) / (x1 - x0);
+          x0 = xmin;
+      }
+      else if (cod0[1]) {
+          y0 += (xmax - x0) * (y1 - y0) / (x1 - x0);
+          x0 = xmax;
+      }
+      else if (cod0[2]) {
+          x0 += (ymin - y0) * (x1 - x0) / (y1 - y0);
+          y0 = ymin;
+      }
+      else if (cod0[3]) {
+          x0 += (ymax - y0) * (x1 - x0) / (y1 - y0);
+          y0 = ymax;
+      }
+      else {
+          return true;
+      }
+  } while (1);
+  return false;
 }
